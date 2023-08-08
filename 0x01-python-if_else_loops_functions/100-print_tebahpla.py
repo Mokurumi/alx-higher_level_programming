a9b83243158766e8f73b15f2ce2bc5625c727c5e
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-08-08 20:48:00 +0000

    alphabet in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 for i in range(90, 64, -1):
-    print('{}'.format(chr(i + 32)), end='') if i % 2 == 0 else print('{}'.format(chr(i)), end='')
+    print('{}'.format(chr(i + 32) if i % 2 == 0 else chr(i)), end='')
+
+print()
