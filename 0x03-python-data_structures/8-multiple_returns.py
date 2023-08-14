a7a13e68d26ae84d2940a076b7ade2a3a3a75bd2
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-08-14 15:41:43 -0700

    correct 7 n 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return None if not sentence else (len(sentence), sentence[0])
+    return (0, None) if not sentence else (len(sentence), sentence[0])
