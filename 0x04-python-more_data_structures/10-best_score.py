872194fa8a9cab1387956e164f446dc164dd30af
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-08-17 11:01:15 -0700

    correct 10 and 12

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,5 +4,5 @@ def best_score(a_dictionary):
         return None
 
     sorted_scores = sorted(a_dictionary.items(),
-            key = lambda x: x[1], reverse = True)
+            key=lambda x: x[1], reverse=True)
     return sorted_scores[0][0]
