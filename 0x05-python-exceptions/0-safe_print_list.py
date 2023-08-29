8825b8081d4be4509f3357a2bb1e582ca440fa00
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-08-28 19:09:58 -0700

    correct 0-2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        for item in my_list:
-            if count < x:
-                print("{}".format(item, end=' '))
-                count += 1
-            else:
-                break
-        print('')
-        return (count)
-    except:
-        return (0)
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except:
+            break
+    print()
+    return (count)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        count = 0
-        int_count = 0
-        
-        for item in my_list:
-            if isinstance(item, int):
-                if int_count < x:
-                    print("{:d}".format(item), end=' ')
-                    int_count += 1
-                else:
-                    break
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
             count += 1
-            if count >= x:
-                break
-        
-        print()
-        return (int_count)
-    except:
-        return (0)
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (count)
