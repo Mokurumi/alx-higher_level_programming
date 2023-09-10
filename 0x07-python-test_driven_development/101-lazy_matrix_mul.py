167f53f37a67956ffc9c7f705b443ee523f82bb1
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-09-10 12:08:30 -0700

    numpy mul

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
+'''module that uses numpy to multiply matrices'''
+
+
 import numpy as np
+
 
 def lazy_matrix_mul(m_a, m_b):
     """
-    Multiply two matrices m_a and m_b using NumPy.
+    Multiply two matrices using NumPy.
 
     Args:
-    m_a (list of lists): The first matrix.
-    m_b (list of lists): The second matrix.
+    m_a (numpy.ndarray): The first matrix.
+    m_b (numpy.ndarray): The second matrix.
 
     Returns:
-    numpy.ndarray: The result of multiplying m_a and m_b as a NumPy array.
-
-    Raises:
-    ValueError: If the matrices
-        cannot be multiplied due to incompatible dimensions.
-
-    Example:
-    >>> lazy_matrix_mul([[1, 2], [3, 4]], [[5, 6], [7, 8]])
-    array([[19, 22],
-           [43, 50]])
+    numpy.ndarray: The result of matrix multiplication.
     """
-    try:
-        np_m_a = np.array(m_a)
-        np_m_b = np.array(m_b)
-        result = np.dot(np_m_a, np_m_b)
-        return result
-    except ValueError as ve:
-        raise ValueError("NumPy matrix multiplication error: " + str(ve))
+    return np.matmul(m_a, m_b)
