b280903ee5785cc625e1a0d416c9541fabba59f7
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-09-13 14:35:11 -0700

    class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+module to convert class to JSON
+"""
+
+def class_to_json(obj):
+    """
+    Converts an object to a dictionary with serializable data structures
+    (list, dictionary, string, integer, and boolean).
+
+    Args:
+        obj: An instance of a Class with serializable attributes.
+
+    Returns:
+        dict: A dictionary representation of the object with serializable data.
+    """
+    return obj.__dict__
