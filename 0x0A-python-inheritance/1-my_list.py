0cd51cb9ab72fbf9dc685dbb48afe0b2ee4a0e00
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-09-12 23:09:19 -0700

    update mylist class

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,11 +3,25 @@
 
 
 class MyList(list):
+    """
+    a class that inherits from list
+    """
+
+    def append(self, item):
+        """
+        Override the append method to add an item to the list.
+
+        Args:
+        item: The item to be added to the list.
+        """
+        super().append(item)  # Call the parent class's append method
+
     def print_sorted(self):
         """
-        Prints the elements of the list in ascending order.
+        Sorts and prints the elements of the list in ascending order.
+
+        Example:
+        my_list = MyList([3, 1, 2, 4, 5])
+        my_list.print_sorted()  # Output: 1 2 3 4 5
         """
-        sorted_list = sorted(self)
-        for item in sorted_list:
-            print(item, end=' ')
-        print()  # Print a newline at the end
+        print(sorted(self))
