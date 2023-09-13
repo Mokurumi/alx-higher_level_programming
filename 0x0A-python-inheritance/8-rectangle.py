c8d1b6ad2d6822161bdbdd4bba4818c1fcdc28cf
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-09-13 02:31:03 -0700

    class rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''module for rectangle class'''
+
+
+class Rectangle(BaseGeometry):
+    '''rectangle class that inherits from BaseGeometry'''
+    def __init__(self, width, height):
+        """
+        Initializes a Rectangle object with the specified width and height.
+
+        Args:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+
+        Raises:
+        TypeError: If width or height is not an integer.
+        ValueError: If width or height is less than or equal to 0.
+        """
+        # Initialize as 0, will be set through integer_validator
+        self.__width = 0
+        # Initialize as 0, will be set through integer_validator
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """
+        Returns a string representation of the Rectangle object.
+
+        Returns:
+        str: A string in the format "[Rectangle] width/height".
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
