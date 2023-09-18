964aa6d9748d9261d7d2e1d0ac90755715a41c7f
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-09-18 13:09:57 -0700

    update square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -34,6 +34,20 @@ class Square(Rectangle):
         # Call the constructor of the Rectangle class with the appropriate args
         super().__init__(size, size, x, y, id)
 
+    # Getter and setter methods for the size attribute.
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
     # Override the __str__ method to customize the string representation.
     def __str__(self):
         """
@@ -43,3 +57,23 @@ class Square(Rectangle):
             str: A string in the format [Square] (<id>) <x>/<y> - <size>.
         """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    # Public method to update the attributes based on provided args or kwargs.
+    def update(self, *args, **kwargs):
+        """
+        Update the attributes of the square based on provided args or kwargs.
+
+        Args:
+            *args: Variable number of arguments in the order: id, size, x, y.
+            **kwargs: Keyword arguments representing attribute names and values
+
+        Returns:
+            None
+        """
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i, value in enumerate(args):
+                setattr(self, attrs[i], value)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
