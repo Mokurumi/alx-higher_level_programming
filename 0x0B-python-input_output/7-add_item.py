6670dc0bf14bf10184ae53f7ca2027ce2a94bf53
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-09-24 15:56:05 -0700

    correct add item from import

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,13 +4,24 @@ module to add all arguments to a Python list, and then save them to a file
 """
 
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_file = __import__('6-load_from_json_file').load_from_json_file
+import json
+import sys
 
+
+# Import the functions using __import__
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+if __name__ == "__main__":
+    # Check if the JSON file exists, and if not, create it
     try:
-        items = load_from_file("add_item.json")
+        existing_data = load_from_json_file('add_item.json')
     except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+        existing_data = []
+
+    # Combine existing data with command line arguments
+    new_data = existing_data + sys.argv[1:]
+
+    # Save the combined data to 'add_item.json'
+    save_to_json_file(new_data, 'add_item.json')
