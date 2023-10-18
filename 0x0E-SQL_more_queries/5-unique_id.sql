572a2b3ad29f173bea210881cecf530dd72f70e1
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-10-18 16:02:06 -0700

    select across tables

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- creates the table unique_id on MySQL server with cols id and name
 CREATE TABLE IF NOT EXISTS `unique_id` (
     `id` INT DEFAULT 1 UNIQUE,
-    `name` VARCHAR(256) NOT NULL
+    `name` VARCHAR(256)
 );
