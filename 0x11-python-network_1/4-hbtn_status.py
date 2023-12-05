d94cbf7601727c2182ccb5eaa1488b652fb5c918
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-12-06 02:09:41 +0300

    status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Python script that fetches https://alx-intranet.hbtn.io/status
+'''
+import requests
+
+
+if __name__ == "__main__":
+    # Send a GET request to the URL using requests
+    URL = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(URL)
+
+    # Display the body of the response in the specified format
+    print("-" * 50)
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
