fd39dc16ff154ffa77455336dd30212b6d1d8b6c
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-12-23 23:21:57 +0300

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const fileA = process.argv[2];
-const fileB = process.argv[3];
-const fileC = process.argv[4];
-const textA = fs.readFileSync(fileA, 'utf8');
-const textB = fs.readFileSync(fileB, 'utf8');
+const args = process.argv.slice(2);
+const fileA = args[0];
+const fileB = args[1];
+const fileC = args[2];
+const textA = fs.readFileSync(fileA, 'utf-8');
+const textB = fs.readFileSync(fileB, 'utf-8');
+
+fs.writeFileSync(fileC, textA + textB);
