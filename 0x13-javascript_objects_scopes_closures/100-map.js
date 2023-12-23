ef448a3f6ed1692ed361e467b369545e3f44bd4f
Alpha Mokurumi <mokurumialpha@gmail.com>
2023-12-23 23:17:12 +0300

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const list = require('./100-data.js');
+const list = require('./100-data').list;
 console.log(list);
-const newList = list.map((value, index) => value * index);
-console.log(newList);
+console.log(list.map((value, index) => value * index));
