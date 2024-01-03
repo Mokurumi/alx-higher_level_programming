74d28817c1700888168535d2ba0a97b405acc676
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-01-04 01:42:36 +0300

    correct chars

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -8,14 +8,19 @@ request(url, function (error, response, body) {
     console.error('error:', error);
   } else {
     const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, function (error, response, body) {
-        if (error) {
-          console.error('error:', error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+    getCharacter(characters, 0);
   }
 });
+
+function getCharacter (characters, index) {
+  request(characters[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+    }
+    if (index + 1 < characters.length) {
+      getCharacter(characters, index + 1);
+    }
+  });
+}
